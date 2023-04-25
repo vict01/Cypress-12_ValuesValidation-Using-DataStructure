e26f87250c4c4e057ff3b99ca5a44e72bf70d739
Victor Caminero <victor.caminero@taxdoo.com>
2023-04-25 08:49:55 +0200

    One more test added

--- cypress/e2e/mainTest.cy.js
+++ cypress/e2e/mainTest.cy.js
@@ -2,14 +2,14 @@
 import { appData, userData } from "../fixtures/fixtures.json"
 
 describe('Business Critical UI Scenario', () => {
-    const urlDatev = Cypress.config().baseUrl+ 'datev_settings.php'
+    const urlDatev = Cypress.config().baseUrl + 'datev_settings.php'
 
     before(() => {
         cy.visit('/')
     })
 
     beforeEach(() => {
-        cy.logInToPage(userData.email, userData.password)        
+        cy.logInToPage(userData.email, userData.password)
         cy.preserveAndAssertCookies()
     })
 
@@ -28,7 +28,21 @@ describe('Business Critical UI Scenario', () => {
                 });
             });
         });
-
     });
 
+})
+
+describe('Business Critical data Scenario', () => {
+    const dataPath = 'cypress/support/data/';
+    it('Validate values in file 1 are contained in file2', () => {
+        cy.readFile(dataPath+'file1.json').then(file1 => {
+            cy.readFile(dataPath+'file2.json').then(file2 => {
+
+                const values1 = file1.values;
+                const values2 = file2.values;
+
+                expect(values1.every(val => values2.includes(val))).to.be.true;
+            });
+        });
+    });
 })
